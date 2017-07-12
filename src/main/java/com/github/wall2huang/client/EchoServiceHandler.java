b9aefb88ae0b203464905686ab7975f875b6779e
wall2huang <sb13590853671>
2017-07-12 21:52:49 +0800

    wall:Ok

---
+++ src/main/java/com/github/wall2huang/client/EchoServiceHandler.java
@@ -0,0 +1,40 @@
+package com.github.wall2huang.client;/**
+ * Created by Administrator on 2017/7/12.
+ */
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * author : Administrator
+ **/
+public class EchoServiceHandler extends ChannelInboundHandlerAdapter
+{
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
+    {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("service received :" + buf.toString(CharsetUtil.UTF_8));
+        ctx.write(buf);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception
+    {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+
+    }
+}
